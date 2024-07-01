0e98ae6adeda79949237cc20eaa8fdc8ebba58d7
diegosmp <diiegosmp@gmail.com>
2024-07-01 02:44:18 -0300

    Add: route and edit Instructor Controller

--- controllers/InstructorController.js
+++ controllers/InstructorController.js
@@ -65,4 +65,55 @@ module.exports = class InstructorController {
       res.status(500).json({ message: 'Erro ao conectar com o servidor!' })
     }
   }
+  static async editInstructor(req, res) {
+    const { id } = req.params
+    const { firstname, lastname, email } = req.body
+
+    console.log(firstname)
+    console.log(lastname)
+    console.log(email)
+
+    if (!firstname) {
+      return res
+        .status(422)
+        .json({ message: 'O campo primeiro nome é obrigatório!' })
+    }
+
+    if (!lastname) {
+      return res
+        .status(422)
+        .json({ message: 'O campo ultimo nome é obrigatório!' })
+    }
+
+    if (!email) {
+      return res.status(422).json({ message: 'O campo e-mail é obrigatório!' })
+    }
+
+    try {
+      validateEmail(email)
+    } catch (error) {
+      return res.status(422).json({ message: error.message })
+    }
+
+    const instructor = await Instructor.findByPk(id)
+
+    if (!instructor) {
+      return res.status(422).json({ message: 'Professor não cadastrado!' })
+    }
+
+    try {
+      await Instructor.update(
+        {
+          firstname,
+          lastname,
+          email,
+        },
+        { where: { _id: id } },
+      )
+
+      res.status(200).json({ message: 'Professor atualizado com sucesso!' })
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao conectar com o servidor!' })
+    }
+  }
 }
--- routes/InstructorRoute.js
+++ routes/InstructorRoute.js
@@ -3,5 +3,6 @@ const InstructorController = require('../controllers/InstructorController')
 const router = require('express').Router()
 
 router.post('/create', InstructorController.createInstructor)
+router.patch('/edit/:id', InstructorController.editInstructor)
 
 module.exports = router
